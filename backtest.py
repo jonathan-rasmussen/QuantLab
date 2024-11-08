5bf02391fb90d1648ef25117e23538545935aa63
Jonathan Rasmussen <jonathan_rasmussen@live.com>
2024-11-08 11:32:03 -0500

    Boiler Plate code for backtest engine

---
+++ backtest.py
@@ -0,0 +1,30 @@
+class Engine():
+    """The engine is the main object that will be used to run our backtest.
+    """
+
+    def __init__(self):
+        pass
+
+
+class Strategy():
+    """This base class will handle the execution logic of our trading strategies
+    """
+
+    def __init__(self):
+        pass
+
+
+class Trade():
+    """Trade objects are created when an order is filled.
+    """
+
+    def __init__(self):
+        pass
+
+
+class Order():
+    """When buying or selling, we first create an order object. If the order is filled, we create a trade object.
+    """
+
+    def __init__(self):
+        pass
